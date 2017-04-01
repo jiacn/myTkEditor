42dbfcbe5af1d89d9a072e1cadf2cc05ced443cd
Jia Zhang <jia.zhang.sh@gmail.com>
2017-04-01 15:57:09 +0800

    Adding Top-level

---
+++ my_editor.py
@@ -0,0 +1,15 @@
+"""
+Text Editor Code
+Step 1: Adding Top-level
+"""
+from tkinter import *
+
+
+PROGRAM_NAME = "My Editor"
+
+root = Tk()
+root.geometry('350x350')
+root.title(PROGRAM_NAME)
+
+root.mainloop()
+
